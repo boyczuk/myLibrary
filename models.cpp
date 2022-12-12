4bb953fc2f50baab3af5f85368b7cf363d30251e
Ian <adlaibridsonboyczuk@gmail.com>
2022-12-11 20:44:12 -0500

    Variable SQL, todo next cli + user input

--- models.cpp
+++ models.cpp
@@ -5,9 +5,6 @@
 
 using namespace std;
 
-
-
-
 static int callback(void *NotUsed, int argc, char **argv, char **azColName)
 {
     int i;
@@ -22,37 +19,52 @@ static int callback(void *NotUsed, int argc, char **argv, char **azColName)
 void addBook()
 {
     sqlite3 *db;
-   char *zErrMsg = 0;
-   int rc;
-   char *sql;
+    char *zErrMsg = 0;
+    int rc;
+    string str_sql;
 
-   /* Open database */
-   rc = sqlite3_open("library.db", &db);
-   
-   if( rc ) {
-      fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
-      return;
-   } else {
-      fprintf(stderr, "Opened database successfully\n");
-   }
+    /* Open database */
+    rc = sqlite3_open("library.db", &db);
 
-   /* Create SQL statement */
-   sql = "INSERT INTO LIBRARY (ID,TITLE,AUTHOR,GENRE,DESCRIPTION,DATE_ADDED) "  \
-         "VALUES (1, 'BAD AS I WANNA BE', 'Dennis Rodman', 'Biography', 'Dennis rodman autobiography', '2022-12-11' ); " \
-         "INSERT INTO LIBRARY (ID,TITLE,AUTHOR,GENRE,DESCRIPTION,DATE_ADDED) "  \
-         "VALUES (2, 'Algorithms are not enough', 'Herbert L. Roitblat', 'Non-fiction', 'Creating General Artificial Intelligence', '2022-12-11' ); ";
+    if (rc)
+    {
+        fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+        return;
+    }
+    else
+    {
+        fprintf(stderr, "Opened database successfully\n");
+    }
 
-   /* Execute SQL statement */
-   rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
-   
-   if( rc != SQLITE_OK ){
-      fprintf(stderr, "SQL error: %s\n", zErrMsg);
-      sqlite3_free(zErrMsg);
-   } else {
-      fprintf(stdout, "Records created successfully\n");
-   }
-   sqlite3_close(db);
-   return;
+    string title = "Crime and Punishment";
+    string author = "Fyodor Dostoevsky";
+    string genre = "Fiction";
+    string description = "moral dilemmas";
+    string date = "2022-12-11";
+
+    // char *str_title = title.c_str();
+
+    /* Create SQL statement */
+    str_sql = "INSERT INTO LIBRARY (ID,TITLE,AUTHOR,GENRE,DESCRIPTION,DATE_ADDED) "
+              "VALUES (4, '" +
+              title + "', '" + author + "', '" + genre + "', '" + description + "', '" + date + "' ); ";
+
+    const char *sql = str_sql.c_str();
+
+    /* Execute SQL statement */
+    rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
+
+    if (rc != SQLITE_OK)
+    {
+        fprintf(stderr, "SQL error: %s\n", zErrMsg);
+        sqlite3_free(zErrMsg);
+    }
+    else
+    {
+        fprintf(stdout, "Records created successfully\n");
+    }
+    sqlite3_close(db);
+    return;
 }
 
 int main(int argc, char *argv[])
@@ -77,11 +89,11 @@ int main(int argc, char *argv[])
 
     /* Create SQL statemednt */
     sql = "CREATE TABLE LIBRARY("
-          "ID INT PRIMARY KEY     NOT NULL," \
-          "TITLE          TEXT    NOT NULL," \
-          "AUTHOR         TEXT    NOT NULL," \
-          "GENRE          TEXT    NOT NULL," \
-          "DESCRIPTION    TEXT," \
+          "ID INT PRIMARY KEY     NOT NULL,"
+          "TITLE          TEXT    NOT NULL,"
+          "AUTHOR         TEXT    NOT NULL,"
+          "GENRE          TEXT    NOT NULL,"
+          "DESCRIPTION    TEXT,"
           "DATE_ADDED     TEXT    NOT NULL );";
 
     /* Execute SQL statement */
